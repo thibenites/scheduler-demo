8dc94ba00db672541c11b507c306b7ac8687c24d
thibenites <thiagolab97@gmail.com>
2017-10-01 12:43:34 -0300

    comemtários adicionados

--- src/main.c
+++ src/main.c
@@ -7,11 +7,11 @@ int main() {
 
   scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
 
-  for (int i=1; i<(n+1); i++) {
-    flag = 0;
-    if(i%a[0] == 0){
-        printf("0");
-        flag = 1;
+  for (int i=1; i<(n+1); i++) { //começo na iteração 1, para facilitar a conta de resto (%), terminando na (n+1), obviamente
+    flag = 0;           //zera a flag no início de cada iteração
+    if(i%a[0] == 0){    //conta para ver se esta tarefa deve ocorrer nesta iteração, caso resto igual a zero, deve acontecer
+        printf("0");    //impressão da tarefa
+        flag = 1;       //flag para inidcar que houve tarefa
     }
     if(i%a[1] == 0){
         printf("1");
@@ -29,7 +29,7 @@ int main() {
         printf("4");
         flag = 1;
     }
-    if(flag == 0){
+    if(flag == 0){      //se não houve tarefa nesta iteração, imprimi-se o hífen, seguido do \n, c.c. só o \n
         printf("-\n");
     }
     else{
