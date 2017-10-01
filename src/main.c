e3c7b8a80469ff6ed2925e94ec054036ad026c1e
thibenites <thiagolab97@gmail.com>
2017-10-01 12:36:01 -0300

    programa funcional

--- src/main.c
+++ src/main.c
@@ -3,11 +3,38 @@
 int main() {
   int a[5];
   int n;
+  int flag; //flag para ver se teve alguma ocorrência na iésima interação
 
   scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
 
-  for (int i=0; i<n; i++) {
-    /* Imprime resultado da n-esima iteracao do scheduler */
+  for (int i=1; i<(n+1); i++) {
+    flag = 0;
+    if(i%a[0] == 0){
+        printf("0");
+        flag = 1;
+    }
+    if(i%a[1] == 0){
+        printf("1");
+        flag = 1;
+    }
+    if(i%a[2] == 0){
+        printf("2");
+        flag = 1;
+    }
+    if(i%a[3] == 0){
+        printf("3");
+        flag = 1;
+    }
+    if(i%a[4] == 0){
+        printf("4");
+        flag = 1;
+    }
+    if(flag == 0){
+        printf("-\n");
+    }
+    else{
+        printf("\n");
+    }
   }
 
   return 0;
